59c634ad7bb938ca3163090ca46c0f42f6cbe921
Aephir <waldenb@gmail.com>
2023-05-29 10:28:52 +0200

    Updated config flow

--- custom_components/ha-ai-electricity-price/config_flow.py
+++ custom_components/ha-ai-electricity-price/config_flow.py
@@ -1,8 +1,6 @@
 from typing import Any, Dict, Optional
 import voluptuous as vol
 from homeassistant import config_entries
-from homeassistant.const import CONF_URL, CONF_TOKEN
-
 from .const import (
     DOMAIN,
     CONF_ELOVERBLIK_TOKEN,
@@ -12,13 +10,12 @@ from .const import (
 
 class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
-    CONNECTION_CLASS = config_entries.CONN_CLASS_CLOUD_POLL  #CONN_CLASS_CLOUD_POLLING
+    CONNECTION_CLASS = config_entries.CONN_CLASS_CLOUD_POLL
 
     async def async_step_user(self, user_input: Optional[Dict[str, Any]] = None) -> Dict[str, Any]:
         errors: Dict[str, str] = {}
         if user_input is not None:
-            if not errors:
-                return await self.async_step_details(user_input)
+            return await self.async_step_details(user_input)
 
         return self.async_show_form(
             step_id="user",
@@ -33,16 +30,15 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     async def async_step_details(self, user_input: Optional[Dict[str, Any]] = None) -> Dict[str, Any]:
         errors: Dict[str, str] = {}
         if user_input is not None:
-            return self.async_create_entry(title="Electricity Price", data=user_input)
+            return self.async_show_form(
+                step_id="details",
+                data_schema=vol.Schema(
+                    {
+                        vol.Required(CONF_ELOVERBLIK_TOKEN): str,
+                        vol.Required(CONF_METERING_POINT): str,
+                    }
+                ),
+                errors=errors,
+            )
 
-        return self.async_show_form(
-            step_id="details",
-            data_schema=vol.Schema(
-                {
-                    vol.Required(CONF_URL, default="https://api.eloverblik.dk/CustomerApi/"): str,
-                    vol.Required(CONF_ELOVERBLIK_TOKEN): str,
-                    vol.Required(CONF_METERING_POINT): str,
-                }
-            ),
-            errors=errors,
-        )
+        return self.async_create_entry(title="Electricity Price", data=user_input)
